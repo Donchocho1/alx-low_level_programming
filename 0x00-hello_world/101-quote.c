04dc576406c045f9bd40dc9250b9c462c9b5815d
Donchocho1 <ogagaoghene11@gmail.com>
2023-08-10 09:16:35 -0700

    First Commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry points
+ *
+ * Reurn: Always 0(Suceess)
+ */
+int main (void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 64);
+	return (1);
+}
