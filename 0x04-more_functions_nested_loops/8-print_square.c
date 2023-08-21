bab017b07176adf0f280b5df5490f093f49a1c66
Donchocho1 <ogagaoghene11@gmail.com>
2023-08-21 14:59:31 -0700

    Third trial

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,14 @@
 void print_square(int size)
 {
 
-int i,j;
+int i, j;
 
 	for (i = 0; i < size; i++)
 	{
-		
+
 		for (j = 0; j < size; j++)
 		{
-			_putchar('#');	
+			_putchar('#');
 		}
 		_putchar('\n');
 	}
