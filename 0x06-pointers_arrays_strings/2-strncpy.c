52600c2a1ef9d3e76609196f524d1c9252bc902a
Donchocho1 <ogagaoghene11@gmail.com>
2023-08-27 10:42:07 -0700

    First commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,16 +15,14 @@ char *_strncpy(char *dest, char *src, int n)
 char *dest_add = dest;
 int i;
 	i = 0;
-	while (*src != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
 		i++;
 	}
 while (i < n)
 {
-*dest = '\0';
+dest[i] = '\0';
 i++;
 }
 return (dest_add);
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,24 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int result;
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 	{
-		if (*s1 > *s2)
-		{
-			result = *s1 - *s2;
-			return (result);
-		}
-		else if (*s1 < *s2)
-		{
-			result = *s1 - *s2;
-			return (result);
-		}
-
-		else
-			return (0);
-	s1++;
-	s2++;
+		s1++;
+		s2++;
 	}
-return (0);
+		if (*s1 == *s2)
+		{
+			return (0);
+		}
+		else
+			return (*s1 - *s2);
+
 }
