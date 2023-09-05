d76f567a39bbf07c96992d117314ce474c579044
Donchocho1 <ogagaoghene11@gmail.com>
2023-09-05 05:18:55 -0700

    First Tasks

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -23,9 +23,9 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	if (size == 0)
-        {
-                return (NULL);
-        }
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
