a30ae3812d52d8e83fd38d0404962e0e3f949e5c
Donchocho1 <ogagaoghene11@gmail.com>
2023-09-11 16:26:55 -0700

    Only Commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print the file name that was compiled
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
