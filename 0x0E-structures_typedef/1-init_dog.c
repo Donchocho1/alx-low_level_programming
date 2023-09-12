c5e24f08aa03b58b47dd47478d9180665996e4f0
Donchocho1 <ogagaoghene11@gmail.com>
2023-09-12 14:37:46 -0700

    First commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - a function that initializes a var of type struct dog
+ * @d: pointer to type struct poiting to struct dog
+ * @name: char pointer to an element of the struct
+ * @age: float value to an element of the struct
+ * @owner: char pointer to an element
+ *
+ * Return: Void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A STRUCT that defines a personalized data type for dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: This struct holds the following elemnet for a dog
+ * the name of the dog, the age of the dog and the owner of
+ * the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /*DOG_H */
