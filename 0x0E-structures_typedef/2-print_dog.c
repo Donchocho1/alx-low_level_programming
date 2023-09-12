124a205824334771a68c0df258dbfa464250b01b
Donchocho1 <ogagaoghene11@gmail.com>
2023-09-12 16:47:05 -0700

    First Commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,8 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
-	}
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
@@ -23,4 +21,6 @@ void print_dog(struct dog *d)
 		printf("Owner:(nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
+
+	}
 }
