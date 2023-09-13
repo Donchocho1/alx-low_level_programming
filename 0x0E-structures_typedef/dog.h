9af15fd8a4450b25804446bf2ff632f74ae9ad3a
Donchocho1 <ogagaoghene11@gmail.com>
2023-09-12 17:56:25 -0700

    First commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,21 +12,13 @@
  * the dog
  */
 
-/*struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-*/
-typedef struct dog
-{
-        char *name;
-        float age;
-        char *owner;
-}dog_t;
+} dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
