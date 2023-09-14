d2a662f81222ef453942e67dc5691ce24e96017a
Donchocho1 <ogagaoghene11@gmail.com>
2023-09-14 11:29:43 -0700

    First Commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -19,7 +19,10 @@ void array_iterator(int *array, size_t size, void (*action)(int num))
 	if (array == NULL)
 		return;
 	if (size <= 0)
+	{	
 		return;
+	}
+	else
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
